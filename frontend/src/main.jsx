0576eb54e5e1eb7b89df9a243243b2656d99f39e
Anamika Verma <anamikatverma888@gmail.com>
2026-05-28 11:24:43 +0530

    fix: use relative import path for ChatProvider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import { ChakraProvider } from '@chakra-ui/react'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
-import  ChatProvider  from '/Users/anamikaverma/chat_app/frontend/src/Context/ChatProvider.jsx'
+import ChatProvider from "./Context/ChatProvider.jsx"
 
 createRoot(document.getElementById('root')).render(
   
